4d4bed540e9cbaade24c7ed0ba306054f860ea20
Charity Rymbai <charityrymbai@hotmail.com>
2025-01-21 22:51:18 +0530

    added delete-user endpoint and made changes in signin endpoint

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
-import { UpdateUserSchema, UserSchema } from '../schema/schema';
+import { DeleteUserSchema, UpdateUserSchema, UserSignInSchema, UserSignUpSchema } from '../schema/schema';
 
 const userRouter = express.Router();
 
@@ -8,11 +8,10 @@ userRouter.get("/health", async (req: Request, res: Response) => {
     return res.status(200).json({message: "User Service is running"});
 });
 
-userRouter.post("/create", async (req: Request, res: Response) => {
+userRouter.post("/signup", async (req: Request, res: Response) => {
     const body = req.body;
-    // console.log(body);
 
-    const parsedBody = UserSchema.safeParse(body);
+    const parsedBody = UserSignUpSchema.safeParse(body);
 
     if (!parsedBody.success) {
         return res.status(400).json({error: parsedBody.error.errors});
@@ -22,13 +21,13 @@ userRouter.post("/create", async (req: Request, res: Response) => {
 
     try {
         
-    await prisma.user.create({
+    const user = await prisma.user.create({
             data: {
                 ...parsedBody.data,
                 phoneNo: Number(parsedBody.data.phoneNo),
             },
         });
-        return res.status(200).json({message: "user created successfully"});
+        return res.status(200).json({message: "user created successfully", user});
     } catch (error) {
         return res.status(500).json({message: "Internal Server Error"});
     } finally {
@@ -38,18 +37,28 @@ userRouter.post("/create", async (req: Request, res: Response) => {
     
 });
 
-userRouter.get("/get-user/:id", async (req: Request, res: Response) => {
-    const params = req.params;
-    console.log(params);
+userRouter.post("/signin", async (req: Request, res: Response) => {
+    const body = req.body;
+
+    const parsedBody = UserSignInSchema.safeParse(body);
+
+    if (!parsedBody.success) { return res.status(400).json({error: parsedBody.error.errors}); }
+
     const prisma = new PrismaClient();
 
     try {
         const user = await  prisma.user.findUnique({
             where: {
-                id: params.id,
+                email: body.email,
+                password: body.password,
+                deleted: false
             },
         });
-        return res.status(200).json({message: "user fetched successfully", data: user});
+
+        if (user === null) {
+            return res.status(400).json({message: "Invalid email or password"});
+        }
+        return res.status(200).json({message: "user signin successfully", user});
     } catch (error) {
         return res.status(500).json({message: "Internal Server Error"});
     } finally {
@@ -82,6 +91,37 @@ userRouter.put("/update-user/:id", async (req: Request, res: Response) => {
             },
         });
         return res.status(200).json({message: "user updated successfully"});
+    } catch (error) {
+        return res.status(500).json({message: "Internal Server Error"});
+    } finally {
+        prisma.$disconnect();
+    }
+
+});
+
+userRouter.delete("/delete-user/:id", async (req: Request, res: Response) => {
+    const params = req.params;
+    const body = req.body;
+
+    const parsedBody = DeleteUserSchema.safeParse(body);
+
+    if (!parsedBody.success) {
+        return res.status(400).json(parsedBody.error.errors);
+    }
+
+    const prisma = new PrismaClient();
+
+    try {
+        await prisma.user.update({
+            where: {
+                id: params.id,
+                password: body.password,
+            },
+            data: {
+                deleted: true,
+            },
+        });
+        return res.status(200).json({message: "user deleted successfully"});
     } catch (error) {
         return res.status(500).json({message: "Internal Server Error"});
     } finally {
--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,6 +1,6 @@
 import zod from 'zod';
 
-export const UserSchema = zod.object({
+export const UserSignUpSchema = zod.object({
   name: zod.string(),
   email: zod.string().email(),
   password: zod.string().min(8),
@@ -12,6 +12,15 @@ export const UpdateUserSchema = zod.object({
   email: zod.string().email().optional(),
   password: zod.string().min(8).optional(),
   phoneNo: zod.string().length(10).optional(),
+});
+
+export const UserSignInSchema = zod.object({
+  email: zod.string().email(),
+  password: zod.string().min(8),
+});
+
+export const DeleteUserSchema = zod.object({
+  password: zod.string().min(8),
 });
 
 export const AdminSchema = zod.object({
